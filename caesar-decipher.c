542b344a4a7029ae7f0d58bf82e6d56fd5d4b64b
allimacse <alan.escamillam@gmail.com>
2018-04-20 13:16:01 -0500

    descipher-caesar

--- caesar-decipher.c
+++ caesar-decipher.c
@@ -7,14 +7,14 @@ int main(int argc, char const *argv[])
 	char alphabeth[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 	FILE *p,*c;
 	int letter;
-	p = fopen("plaintext.txt","r");
-	c = fopen("ciphertext.txt","w");
-	while(!feof(p)){	
-		letter = fgetc(p)-97;
+	p = fopen("plaintext2.txt","w");
+	c = fopen("ciphertext.txt","r");
+	while(!feof(c)){	
+		letter = fgetc(c)-97;
 		if ((int)letter>=0)
 		{
-			letter=(letter+3)%26;
-			fputc(alphabeth[letter], c);
+			letter=(letter-3+26)%26;
+			fputc(alphabeth[letter], p);
 		}
 		
 	}
